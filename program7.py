e3d1b83629eac91bb4f4c0eaa3bf3630a4c4199a
Marcos <marcosf9@hotmail.com>
2017-07-01 18:52:31 -0300

    Se agrega programa7 con mas funcionalidades

--- program7.py
+++ program7.py
@@ -7,17 +7,31 @@ while True:
         break
     if name in catNames:
         print('el valor ya existe')
+        print('el valor fue ingresado en la ' + str(int(catNames.index(name)) + 1) + ' vez')
         continue
     else:
         print('no hay gatos con nombre ' + name)
-    catNames = catNames + [name] # list concatenation
+    catNames += [name] # list concatenation
 print('The cat names are:')
 for nam in catNames:
     print('  ' + nam)
 if 'gato3' not in catNames:
     print('---> gato3 no esta en la lista')
 else:
-    print('lista completa')
+    print('gato 3 esta en la lista')
+    agregar = input('lista completa, quiere ingregar alguno mas (s/n)?')
+    if agregar == 's':
+        
+        print('agregue el faltante: ', end='\n')
+        name2 = input()
+        print('en que orden? ')
+        ordenname2 = input()
+        catNames.insert(int(ordenname2) - 1, name2)
+    else:
+        print('lista cerrada')
+if len(catNames) > 3:
+    print('cantidad de gatos mayor a 3')
+print('el gato numero 3 es ' + catNames[2])
 
 
 dogsNames = []
@@ -43,3 +57,4 @@ print("-" * 30)
 
 for i in range(len(pets)):
     print('Index ' + str(i) + ' in supplies is: ' + pets[i])
+
